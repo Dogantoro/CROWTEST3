d0984ff8439f3218c5cb7518767e94c4adf53845
dogan <dogantoro@gmail.com>
2024-04-08 22:48:15 -0400

    created testing services and fixed gitignore

--- src/test.cpp
+++ src/test.cpp
@@ -22,6 +22,16 @@ int main()
         auto page = crow::mustache::load_text("css/style.css");
         return page;
     });
+    
+    CROW_ROUTE(app, "/getservice").methods("POST"_method)([](crow::response &res){
+        res.set_static_file_info("templates/getservice.dat");
+        res.set_header("content-type", "image/jpeg");
+        res.end();
+    });
+
+    CROW_ROUTE(app, "/getservice")([](){
+        return "You're Not Supposed To Be Here!";
+    });
 
     CROW_ROUTE(app, "/drug").methods("POST"_method)([](const crow::request& req, crow::response &res) {
         auto x = req.get_body_params();
@@ -32,6 +42,10 @@ int main()
             return;
         }
         auto z = std::string(y);
+        if (z == "\u0061\u006D\u0061\u006E") {
+            res.redirect("/getservice");
+            res.end();
+        }
         auto w = boost::urls::encode(z, boost::urls::unreserved_chars);
         res.redirect("/drug/" + w);
         res.end();
