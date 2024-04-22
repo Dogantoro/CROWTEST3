0125cac4679b318697b0a6d4beb27ec7ad53ec99
emma-coronado <emma.dz.coronado@gmail.com>
2024-04-22 17:12:23 -0400

    add js to display search time

--- templates/scripts/drug.js
+++ templates/scripts/drug.js
@@ -17,6 +17,7 @@ async function loadJson() {
 
     // insert side card content
     document.getElementById("drugName").innerHTML = drugData.drugName;
+    document.getElementById("search-time").insertAdjacentHTML("afterend", drugData.searchTime);
 
     // generate drug interaction code to return
     var majorInter = "";
