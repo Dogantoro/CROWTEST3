c04f1210d7ea8c7ad1d0c5cd1bd20991b7b97974
emma-coronado <emma.dz.coronado@gmail.com>
2024-04-21 21:22:57 -0400

    make dropdown to put data structure toggle on

--- templates/scripts/index.js
+++ templates/scripts/index.js
@@ -1,5 +1,5 @@
 let searchAlert = `
-<div class="home-card alert alert-dark alert-dismissible fade show col-10 m-auto" role="alert">
+<div class="home-card alert alert-dark alert-dismissible fade show col-10 m-auto mt-3" role="alert">
       <i class="bi bi-exclamation-circle-fill me-1"></i>
       <strong>Search not Found</strong> - check your input and try again!
       <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
